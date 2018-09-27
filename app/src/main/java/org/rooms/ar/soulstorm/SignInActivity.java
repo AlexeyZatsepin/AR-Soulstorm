0ade3ce4268293827c62bbc65934f3d4eca55d98
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-09-28 00:10:27 +0300

    Signin (#3)
    * Create activity_sign_in.xml
    
    * Create SignInActivity.java
    
    * Create DrawingView.java
    
    * add colors
    
    * list-layout button
    
    * remove captures

--- app/src/main/java/org/rooms/ar/soulstorm/SignInActivity.java
+++ app/src/main/java/org/rooms/ar/soulstorm/SignInActivity.java
@@ -4,21 +4,38 @@ import android.content.res.Configuration;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.transition.Scene;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.IOException;
 
 public class SignInActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
     private Camera mCamera;
-    private TextureView mTextureView;
+
+    private Scene mSceneLogin;
+    private Scene mSceneRegistration;
+    private Scene mScenePasswordReset;
+    private ViewGroup mSceneRoot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mTextureView = findViewById(R.id.camera_preview);
+        TextureView mTextureView = findViewById(R.id.camera_preview);
         mTextureView.setSurfaceTextureListener(this);
+
+        mSceneRoot = findViewById(R.id.scene_root);
+
+        findViewById(R.id.create_account).setOnClickListener(v -> TransitionManager.go(mSceneRegistration));
+
+        mSceneLogin = Scene.getSceneForLayout(mSceneRoot, R.layout.scene_login, getApplicationContext());
+        mSceneRegistration = Scene.getSceneForLayout(mSceneRoot, R.layout.scene_registration, getApplicationContext());
+        mScenePasswordReset = Scene.getSceneForLayout(mSceneRoot, R.layout.scene_forgot_password, getApplicationContext());
     }
 
     @Override
